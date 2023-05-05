df38829ce4767fa9212861f48745762809aab905
Aleksei Kruglykhin <mj669@yandex.ru>
2023-05-05 19:14:34 +0300

    change port

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const limiter = rateLimit({
   max: 1000,
 });
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
